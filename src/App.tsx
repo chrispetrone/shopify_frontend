98be24eb09316c49cc56810bcdb3ebeea97c04a7
Chris <ctpetrone@gmail.com>
2022-05-18 12:27:00 -0700

    Foundation for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,139 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import { Button, TextField } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Card from "@mui/material/Card";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface AppProps { }
+interface History {
+  prompt: String,
+  response: String
+}
+interface AppState { value: String, history: Array<History> }
+class App extends React.Component<AppProps, AppState> {
+
+  constructor(props: any) {
+    super(props);
+
+    this.state = { value: "Input Text Here", history: [] };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  };
+  handleChange(event: any): any {
+    this.setState({ value: event.target.value });
+  }
+  handleSubmit() {
+    console.log("submit", this.state.value)
+    const input = this.state.value || "";
+    const data = {
+      prompt: input,
+      temperature: 0.5,
+      max_tokens: 64,
+      // top_p: 1.0,
+      // frequency_penalty: 0.0,
+      // presence_penalty: 0.0,
+    };
+    const key: String = "";
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${key}`
+
+    }
+    axios.post(
+      "https://api.openai.com/v1/engines/text-curie-001/completions",
+      data,
+      { headers }
+    ).then(resp => {
+      console.log(resp)
+      this.handleResponse(resp.data.choices[0].text)
+    }).catch(error => {
+      console.log(error.response)
+    });
+
+  }
+
+  handleResponse(text: String) {
+    console.log(text)
+    this.state.history.push({
+      prompt: this.state.value,
+      response: text
+    })
+    this.forceUpdate();
+  }
+
+
+  submitText = (event: any) => {
+    console.log(event);
+  }
+  render() {
+    return (
+      <Box>
+        <Stack
+          spacing={2}
+          alignItems="center"
+          direction="column">
+          <div>
+            <h1>
+              AI Text Auto-Generation
+            </h1>
+          </div>
+          <Grid container width={0.5}>
+            <TextField
+              id="outlined-textarea"
+              placeholder="Enter Text Prompt"
+              label="Enter Text Prompt"
+              multiline
+              onChange={this.handleChange}
+              onKeyDown={(e) => {
+                if (e.key == 'Enter') {
+                  this.handleSubmit()
+                }
+              }}
+              rows={4}
+              sx={{ width: 1 }}
+              margin="normal"
+            />
+          </Grid>
+          <Grid item>
+            <Button
+              type="submit"
+              value="Submit"
+              variant="contained"
+              onClick={(event: any) => {
+                this.handleSubmit()
+              }}>
+              Submit
+            </Button>
+          </Grid>
+          <Grid item>
+            <h4>Responses</h4>
+          </Grid>
+          {this.state.history.map((e, i) =>
+            <Card sx={{ bgcolor: "lightgray", width: 0.5 }} key={i}>
+              <Grid container spacing={1}>
+                <Grid item xs={2}>
+                  <p><b>Prompt:</b></p>
+                </Grid>
+                <Grid item xs={10}>
+                  <p>{this.state.history[i].prompt}</p>
+                </Grid>
+                <Grid item xs={2}>
+                  <p><b>Response:</b></p>
+                </Grid>
+                <Grid item xs={10}>
+                  <p>{this.state.history[i].response}</p>
+                </Grid>
+              </Grid>
+            </Card>
+          )}
+        </Stack >
+      </Box >
+    );
+  }
 }
 
 export default App;
