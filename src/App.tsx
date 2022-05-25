987fcd2a755c832026aa1f618fc435982325eb65
Chris <ctpetrone@gmail.com>
2022-05-25 12:19:28 -0700

    added dropdown for engine selection.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
 import React from 'react';
 import './App.css';
 import axios from "axios";
+import { AxiosRequestHeaders } from 'axios';
 import { Button, TextField } from "@mui/material";
 import Grid from "@mui/material/Grid";
 import Stack from "@mui/material/Stack";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
 
 interface AppProps { }
 interface History {
   prompt: String,
-  response: String
+  response: String,
 }
-interface AppState { value: String, history: Array<History> }
+interface Engines {
+  name: String,
+  desc: String
+}
+
+interface AppState { value: String, history: Array<History>, headers: AxiosRequestHeaders, engines: Array<Engines>, engine: Engines }
 class App extends React.Component<AppProps, AppState> {
 
   constructor(props: any) {
     super(props);
 
-    this.state = { value: "Input Text Here", history: [] };
+    const key: String = `${process.env.REACT_APP_OPENAI_KEY}` || "NO KEY";
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${key}`
+    }
+
+    const engines = [
+      { name: 'text-curie-001', desc: "2/4 - faster & lower cost than Davinci" },
+      { name: 'text-davinci-002', desc: "1/4 - most capable GPT-3 model." },
+      { name: 'text-babbage-001', desc: "3/4 - very fast & low cost" },
+      { name: 'text-ada-001', desc: "4/4 - fastest & Simplest Model" }
+    ]
+    this.state = {
+      value: "Input Text Here",
+      history: [],
+      headers: headers,
+      engines: engines,
+      engine: engines[0]
+
+    };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.changeEngine = this.changeEngine.bind(this);
   };
   handleChange(event: any): any {
     this.setState({ value: event.target.value });
@@ -38,8 +66,11 @@ class App extends React.Component<AppProps, AppState> {
       Authorization: `Bearer ${key}`
 
     }
+
+    console.log(`https://api.openai.com/v1/engines/${this.state.engine.name}/completions`)
+
     axios.post(
-      "https://api.openai.com/v1/engines/text-curie-001/completions",
+      `https://api.openai.com/v1/engines/${this.state.engine.name}/completions`,
       data,
       { headers }
     ).then(resp => {
@@ -50,13 +81,16 @@ class App extends React.Component<AppProps, AppState> {
     });
 
   }
-
   handleResponse(text: String) {
     this.state.history.unshift({
       prompt: this.state.value,
       response: text
     })
     this.forceUpdate();
+  }
+
+  changeEngine(event: any) {
+    this.setState({ engine: this.state.engines[event.target.value] });
   }
 
 
@@ -72,6 +106,24 @@ class App extends React.Component<AppProps, AppState> {
               AI Text Auto-Generation
             </h1>
           </div>
+          <Grid container
+            alignItems="center"
+            justifyContent="center">
+            <Grid item xs={2}>
+              <Select
+                label="engines"
+                id="engines"
+                defaultValue={0}
+                onChange={this.changeEngine}>
+                {this.state.engines.map((e, i) =>
+                  <MenuItem key={i} value={i}> {e.name}</MenuItem>
+                )}
+              </Select>
+            </Grid>
+            <Grid item xs={2}>
+              <p>{this.state.engine.desc}</p>
+            </Grid>
+          </Grid>
           <Grid container width={0.5}>
             <TextField
               id="outlined-textarea"
